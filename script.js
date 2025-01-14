1ee2f7279fb7ad20d4484ace17a16930ffb0afe4
manuel lantieri <manuel.lantieri02@gmail.com>
2025-01-14 07:50:42 +0100

    fix link API

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // dom
 
 const cardElm = document.getElementById("cards-containers")
-const overlayElm = document.getElementById("over-img-container")
-axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6').then(function(response) {
-    response.data.array.forEach((element,index) => {
-        const title = element.title;
-        const url = element.url;
+const overlayElm = document.getElementById("cover-img-container")
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(function(response) {
+    response.data.array.forEach((element, index) => {
+        const titleResult = element.title;
+        const urlResult = element.url;
         const overId = `overlayElm-${index}`;
 
         cardElm.innerHTML +=
@@ -16,17 +16,18 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6').then(function(
                 </div>
                 <div class="my-img-container">
                     <a href="#${overId}">
-                        <img class="ms-img-blog" src="${url}" alt="photo">
+                        <img class="ms-img-blog" src="${urlResult}" alt="photo">
                     </a>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">${title}</p>
+                    <p class="card-text">${titleResult}</p>
                 </div>
             </div>
         </div>`
         
     });
-
-
-    
 })
+
+    .catch(function(error) {
+        console.log(errore)
+    })
